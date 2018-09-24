b3920a157d8305fc76e980674c4dd93f9ba25132
joepal <joe.palomino50@gmail.com>
2018-09-23 22:22:04 -0600

    add automatic testing #16

--- src/components/header.test.js
+++ src/components/header.test.js
@@ -7,6 +7,6 @@ const Title = () => <h1 data-testid="hero-title">Gatsby is awesome!</h1>;
 test('Displays the correct title', () => {
   const { getByTestId } = render(<Title />);
   // Assertion
-  expect(getByTestId('hero-title')).toHaveTextContent('wrong');
+  expect(getByTestId('hero-title')).toHaveTextContent('test');
   // --> Test will pass
 });
